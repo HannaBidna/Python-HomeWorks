bea0353e6215bcded44dfdffe9c41c7ac4a5bbc5
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2023-09-23 20:58:55 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ HW 15_1.py
@@ -0,0 +1,33 @@
+def cyfra(a):
+    if a.lstrip('-').isdigit() is True:
+        num = int(a.lstrip('-'))
+        if num == 0:
+           expl = ''
+        elif num > 0:
+           expl = 'позитивне ціле'
+        else:
+           expl = 'негативне ціле'
+    else:
+        try:
+            a = a.replace(',', '.')
+            num = float(a)
+            if num > 0:
+                expl = 'позитивне дробове'
+            else:
+                expl = 'негативне дробове'
+        except ValueError:
+             expl = 'не'
+    return (f'Ви ввели {expl} число: {a}')
+
+
+def main():
+    while True:
+        print ('?')
+        a = input()
+        if a.upper() in ['ВИХІД', 'EXIT', 'QUIT', 'E', 'Q']:
+            break
+        else:
+            cyfra(a)
+            print(cyfra(a))
+
+main()
