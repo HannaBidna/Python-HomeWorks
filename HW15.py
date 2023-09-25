127a498381a097500f686ef4c3871b03a2ffaae2
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2023-09-25 20:50:58 +0200

    Merge branch 'master' of https://github.com/HannaBidna/Python-HomeWorks into main

--- HW15.py
+++ HW15.py
@@ -1,4 +1,5 @@
 def cyfra(a):
+    a = a.replace(',', '.')
     if a.isdigit() is True:
         a = int(a)
         if a == 0:
@@ -8,16 +9,19 @@ def cyfra(a):
     else:
         if a.startswith('-'):
             if a[1:len(a)].isdigit() is True and int(a[1:len(a)]) != 0:
-               expl = 'негативне ціле'
-               a = int(a.lstrip())
+                expl = 'негативне ціле'
+                a = int(a.lstrip())
+            elif a.count('.') == 0 and a.count(','):
+                expl = 'не'
             else:
-                a = a.replace(',', '.')
+
                 b = a.split('.', 1)
-                print(b)
-                if b[0].strip('-').isdigit() is True and b[1].isdigit() is True:
+                #print(b)
+                if b[0].lstrip('-').isdigit() is True and b[1].isdigit() is True:
                     expl = 'негативне дробове'
                 else:
                     expl = 'не'
+
         else:
             a = a.replace(',', '.')
             parts = a.split('.', 1)
