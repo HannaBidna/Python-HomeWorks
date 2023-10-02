90e9a61cb75e9a2012e020fbe4c50b3ed43d478d
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2023-10-02 12:53:05 +0200

    Merge branch 'master' of https://github.com/HannaBidna/Python-HomeWorks into main

--- HW21.py
+++ HW21.py
@@ -1,17 +1,15 @@
 class Auto:
-    cоlor = blue
-    weight = 2
-
-    def __init__(self, brand, age, mark):
+    def __init__(self, brand, age, mark, cоlor='blue', weight=2):
         self.brand = brand
         self.age = age
         self.mark = mark
 
     def move(self):
-        print('Mowe')
+        print('Move')
 
     def stop(self):
         print('Stop')
 
     def birthday(self):
         self.age += 1
+
