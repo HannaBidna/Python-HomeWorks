f0cac755ed97c7ed5a416d7767a88f01310ff963
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2023-10-16 21:49:12 +0200

    Merge branch 'master' of https://github.com/HannaBidna/Python-HomeWorks into main

--- 1.py
+++ 1.py
@@ -1,8 +1,6 @@
 
 from datetime import date
-
 import openpyxl
-import Diploma_1
 from Diploma_1 import Person
 
 
@@ -14,32 +12,21 @@ def load_data(filename):
     data = []
     wb = openpyxl.load_workbook(filename)
     sh = wb.active
-    for row in sh.iter_rows(values_only=True):
-        p = Person(*row)
-        data.append(p)
-    return data
-
-
-def open_file():
-    wb = openpyxl.load_workbook('test.xlsx')
-    sheet = wb.active
-    rows = sheet.max_row
-    cols = sheet.max_column
-    data = []
-    for row in range(2, rows + 1):
+    rows = sh.max_row
+    cols = sh.max_column
+    for row in range(1, rows + 1):
         string = ''
         for col in range(1, cols + 1):
-            cell = sheet.cell(row=row, column=col)
+            cell = sh.cell(row=row, column=col)
             string += str(cell.value) + ' '
         data.append(string.strip())
         return data
     print(data)
-    wb.save('test.xlsx')
-    wb.close
+    wb.save(filename)
 
 
 def create_file(filename, data):
-    wb = openpyxl.workbook()
+    wb = openpyxl.Workbook()
     sh = wb.active
     for person in data:
         sh.append([person.name, person.birth_date, person.death_date, person.gender, person.surname, person.second_name])
@@ -48,6 +35,7 @@ def create_file(filename, data):
 
 def search(data, word):
     results = []
+
     for person in data:
         if word.lower() in person.name.lower() or word.lower() in (person.surname or '').lower() or word.lower() in (person.second_name or '').lower():
             results.append(person)
@@ -57,37 +45,37 @@ def search(data, word):
 
 
 def main():
-    if __name__ == '__main__':
-        while True:
-            people = []
-            print('Виберіть необхідний пункт меню:', '1 - Завантажити дані ', '2 - Створити новий файл',
+   while True:
+        people = []
+        print('Виберіть необхідний пункт меню:', '1 - Завантажити дані ', '2 - Створити новий файл',
               '3 - Додати запис', '4 - Знайти дані', '5 - Вихід', sep='\n')
-            answer = input()
-            if answer == '1':
-                filename = str(input("Введіть назву файлу: "))
-                people = load_data(filename)
-            elif answer == '2':
-                filename = str(input("Введіть назву файлу: "))
-                create_file(filename, people)
-            elif answer == '3':
-                name = input("Введіть імя: ")
-                birth_date = input("Введіть дату народження: ")
-                death_date = input("Введіть дату смерті (за наявності): ")
-                gender = input("Введіть стать: ")
-                surname = input("Введіть прізвище (за бажанням): ")
-                second_name = input("Введіть по-батькові (за бажанням): ")
-                p = Person(name, birth_date, death_date, gender, surname,  second_name)
-                people.append(p)
-            elif answer == '4':
-                word = input("Введіть слово для пошуку: ")
-                results = search(people, word)
-                for person in results:
-                    print(
-                        f"{person.name} {person.second_name or ''} {person.surname or ''} {person.calculate_age()}років, {person.gender}. Народився: {person.birth_date}. Помер: {person.death_date or '--'}")
-            elif answer == '5':
-                break
-            else:
-                print('Помилка!')
+        answer = input()
+        if answer == '1':
+            filename = str(input("Введіть назву файлу: "))
+            people = load_data(filename)
+        elif answer == '2':
+            filename = str(input("Введіть назву файлу: "))
+            create_file(filename, people)
+        elif answer == '3':
+            name = input("Введіть імя: ")
+            birth_date = input("Введіть дату народження: ")
+            death_date = input("Введіть дату смерті (за наявності): ")
+            gender = input("Введіть стать: ")
+            surname = input("Введіть прізвище (за бажанням): ")
+            second_name = input("Введіть по-батькові (за бажанням): ")
+            p = Person(name, birth_date, death_date, gender, surname,  second_name)
+            people.append(p)
+        elif answer == '4':
+            word = input("Введіть слово для пошуку: ")
+            results = search(people, word)
+            for person in results:
+                print(
+                        f'{person.name} {person.second_name} {person.surname} {person.calculate_age()} років, {person.gender}. Народився: {person.birth_date}. Помер: {person.death_date}')
+        elif answer == '5':
+            break
+        else:
+            print('Помилка!')
 
 
-main()
+if __name__ == '__main__':
+    main()
